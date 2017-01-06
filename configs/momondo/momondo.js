0eb32c4cdeca9378deabf8e5ddff24c8eb9867ae
Carson Huang <carsonhuang2010@gmail.com>
2017-01-06 02:51:40 -0500

    Working project. Ton of garbage code
    Following commits will clean up the project

--- configs/momondo/momondo.js
+++ configs/momondo/momondo.js
@@ -1,18 +1,19 @@
-var configs = require('configs.js');
+var path = require('path');
+var configs = require( path.resolve( __dirname, "configs.js" ));
 
 /**
 * opts = {
 * 	departureSegments = [], // ICAO codes (ie: YUL == Montreal Trudeau Airport)
 *	arrivalSegments = [], //idem as departureSegments
 * 	tripType =  int // 1 = One way, 2 = Return trip, 4 = Multi City
-* 	departureDates = [], // more than one if tripType == 4
-* 	returnDates = [], //more than one if tripType == 4
+* 	departureDates = [], // DD-MM-YYYY; more than one if tripType == 4
+* 	returnDate = DD-MM-YYYY, // used only if tripType == 2
 * 	adults = int
 * }
 * TODO in the future: Ticket class, Direct preferred, include nearby airports, # of childs
 */
 
-function getResults(opts)
+var getResults = function(opts)
 {
 	// if(!opts.segments || !opts.tripType || !opts.departureDates || !opts.returnDates || !opts.adults) {
 	// 	console.log("Error. Missing field in `opts` parameter.");
@@ -20,6 +21,7 @@ function getResults(opts)
 	// }
 
 	var url = urlSetup(opts);
+	return url;
 
 	function urlSetup()
 	{
@@ -36,6 +38,8 @@ function getResults(opts)
 			console.log("Invalid tripType.");
 			return;
 		}
+
+		return url + configs.MOMONDO_FLIGHTS_SEARCH_URL_SEARCH_QUERY_SUFFIX;
 	}
 
 	function getOneWaySearchQuery(){
@@ -46,5 +50,24 @@ function getResults(opts)
 					+ "&SD0=" + opts.arrivalSegments[0]
 					+ "&SDP0=" + opts.departureDates[0]
 					+ "&AD=" + opts.adults;
+
+		return searchQuery;
+	}
+
+	function getReturnSearchQuery(){
+		var searchQuery = "";
+		searchQuery += "&TripType=" + opts.tripType
+					+ "&SegNo=2"
+					+ "&SO0=" + opts.departureSegments[0]
+					+ "&SD0=" + opts.arrivalSegments[0]
+					+ "&SDP0=" + opts.departureDates[0]
+					+ "&SO1=" + opts.arrivalSegments[0]
+					+ "&SD1=" + opts.departureSegments[0]
+					+ "&SDP1=" + opts.returnDate
+					+ "&AD=" + opts.adults;
+
+		return searchQuery;
 	}
 }
+
+module.exports.getResults = getResults;
--- index.js
+++ index.js
@@ -1,26 +1,70 @@
+var jsdom = require('jsdom');
+var $ = null;
+
+jsdom.env(
+ "http://quaintous.com",
+ function (err, window) {
+   $ = require('jquery')(window);
+ }
+);
 var express = require('express')
 var app = express()
+
 var phantom = require('phantom')
+
+var momondo = require('./configs/momondo/momondo.js');
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-	var sitepage = "http://www.w3schools.com/";
+  	var opts = {
+  		tripType : 1,
+  		departureSegments : ["YUL"],
+  		arrivalSegments : ["TYO"],
+  		departureDates : ["25-01-2017"],
+  		adults : 1
+  	};
+
+	var sitepage = momondo.getResults(opts);
+	//sitepage = "http://www.w3schools.com/";
 	var phInstance = null;
 
-	phantom.create()
+	phantom.create(["--ssl-protocol=any"])
 	.then(instance => {
     	phInstance = instance;
     	return instance.createPage();
 	})
 	.then(page => {
-		console.log('success');
+		// page.property('onResourceError', function(resourceError) {
+		//     //console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
+  // 			//console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString)
+		// });
 
+		console.log('success');
+		console.log(sitepage);
 		page.open(sitepage).then(function(status){
-			page.evaluate(function() {
-			    return document.title;
-			}).then(function(html){
-			    console.log(html);
-			});
+			console.log(status);
+			global.setTimeout(function () {
+				console.log('page opened');
+				page.evaluate(function() {
+					var lol = document.querySelectorAll('[data-flight-pos="1"]')[0];
+
+					//console.log("TITLE: " + a);
+					// var mainDiv = $("#results-tickets").find("div[data-flight-pos='0']");
+					//var priceDiv = $(lol).find(".price-pax");
+					//var price = $(priceDiv).find(".value").innerHTML;
+					//console.log("PRICE: " + lol.innerHTML);
+					//console.log('done');
+				    return lol.innerHTML;//document.title;
+				}).then(function(html){
+					var doc = $.parseHTML(html);
+					var priceDiv = $(doc).find(".price-pax")[0];
+					var price = $(priceDiv).find(".value")[0].innerHTML;
+					console.log("PRICE: " + price);
+					console.log('done');
+				});
+			}, 20000);
+
+			
 		});
 		
 		console.log('done');
@@ -30,6 +74,8 @@ app.get('/', function (req, res) {
     	console.log(error);
     	phInstance.exit();
 	});
+
+	//phantom.kill();
 })
 
 app.listen(3000, function () {
