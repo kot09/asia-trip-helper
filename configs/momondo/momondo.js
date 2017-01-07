c50593756f410b9eb1f248735d3482c3946630e6
Carson Huang <carsonhuang2010@gmail.com>
2017-01-07 00:04:27 -0500

    Refactoring: remove console.logs & commented code

--- configs/momondo/momondo.js
+++ configs/momondo/momondo.js
@@ -1,20 +1,16 @@
 /* start jQuery setup */
-
 var jsdom = require('jsdom');
 var $ = null;
 
-jsdom.env(
- "http://quaintous.com",
- function (err, window) {
-   $ = require('jquery')(window);
- }
+jsdom.env("", function (err, window) {
+   		$ = require('jquery')(window);
+ 	}	
 );
-
 /* end jQuery */ 
 
 var path = require('path');
-var configs = require( path.resolve( __dirname, "configs.js" ));
-var phantom = require('phantom')
+var configs = require(path.resolve(__dirname, "configs.js" ));
+var phantom = require('phantom');
 
 var Momondo = function(){};
 
@@ -44,36 +40,20 @@ Momondo.prototype.getLowestPrice = function(opts){
   // 			//console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString)
 		// });
 
-		console.log('success');
-		console.log(sitepage);
-		page.open(sitepage).then(function(status){
-			console.log(status);
+		page.open(sitepage)
+		.then(function(status){
 			global.setTimeout(function () {
-				console.log('page opened');
 				page.evaluate(function() {
 					var lol = document.querySelectorAll('[data-flight-pos="0"]')[0];
 
-					//console.log("TITLE: " + a);
-					// var mainDiv = $("#results-tickets").find("div[data-flight-pos='0']");
-					//var priceDiv = $(lol).find(".price-pax");
-					//var price = $(priceDiv).find(".value").innerHTML;
-					//console.log("PRICE: " + lol.innerHTML);
-					//console.log('done');
-				    return lol.innerHTML;//document.title;
+				    return lol.innerHTML;
 				}).then(function(html){
 					var doc = $.parseHTML(html);
 					var priceDiv = $(doc).find(".price-pax")[0];
 					var price = $(priceDiv).find(".value")[0].innerHTML;
-					console.log("PRICE: " + price);
-					console.log('done');
 				});
 			}, 20000);
-
-			
 		});
-		
-		console.log('done');
-
 	})
 	.catch(error => {
     	console.log(error);
