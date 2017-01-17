4e282e7c7c0e465bfdbbf3c1d73cbb9dfd93ccfa
Carson Huang <carsonhuang2010@gmail.com>
2017-01-17 18:47:39 -0500

    Remove express from code
    Actually useless

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-var express = require('express')
-var app = express()
-var p = require('./usage.js');
-
-app.get('/', function (req, res) {
-  	res.send('Hello World!');
-  	p.startSearch();
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var asiaTripHelper = require('./usage.js');
+asiaTripHelper.startSearch();
